0f63ad3803043f680dd038ea0b2ca2eec1114364
thiagouvea <thiagouvea90@gmail.com>
2025-10-24 12:59:26 -0300

    colocando sombras

--- scenes/light.gd
+++ scenes/light.gd
@@ -7,9 +7,12 @@ extends Node2D
 			type = value
 			for child in $Options.get_children():
 				child.hide()
-			var child = $Options.get_child(int(type))
-			child.show()
-			
+			$Options.get_child(int(type)).show()
+@export_color_no_alpha var color = Color(1.0, 1.0, 1.0):
+	set(value):
+		if get_child_count() > 0 and value != null:
+			color = value
+			$Options.get_child(int(type)).get_child(1).color = value
 
 func _process(_delta: float) -> void:
 	pass
