a6c9b30d688e603cc36c339559020d148f195768
thiagouvea <thiagouvea90@gmail.com>
2025-10-23 22:01:20 -0300

    adcionando luzes e sombras

--- scenes/light.gd
+++ scenes/light.gd
@@ -1,5 +1,15 @@
 @tool
 extends Node2D
 
+@export_enum('0', '1', '2') var type = '0':
+	set(value):
+		if get_child_count() > 0 and value != null:
+			type = value
+			for child in $Options.get_children():
+				child.hide()
+			var child = $Options.get_child(int(type))
+			child.show()
+			
+
 func _process(_delta: float) -> void:
 	pass
